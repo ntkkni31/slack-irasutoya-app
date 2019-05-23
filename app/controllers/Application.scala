4e9d9e799c08470926f816cef230778c9d0e529b
Natsuki Kanai <natsuki-kanai@kccs.co.jp>
2019-05-23 20:49:00 +0900

    support multi search keyword

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.net.URLEncoder
+
 import javax.inject.{Inject, Singleton}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import play.api._
@@ -31,7 +33,9 @@ class Application @Inject()(cc: ControllerComponents, ws: WSClient) extends Abst
           val keyword = x("text").head
           val responseUrl = x("response_url").head
 
-          respondCommand(keyword, responseUrl)
+          if(keyword != null && keyword.length > 0) {
+            respondCommand(keyword, responseUrl)
+          }
 
         case None =>
       }
@@ -45,65 +49,69 @@ class Application @Inject()(cc: ControllerComponents, ws: WSClient) extends Abst
   }
 
   private def respondCommand(keyword: String, responseUrl: String):Unit = Future {
-    val browser = JsoupBrowser()
-    val doc = browser.get("https://www.irasutoya.com/search?q=" + keyword)
+    try {
+      val browser = JsoupBrowser()
+      val doc = browser.get("https://www.irasutoya.com/search?q=" + URLEncoder.encode(keyword, "UTF-8") )
 
-    val items = doc >> elementList("div.date-outer div.boxim")
+      val items = doc >> elementList("div.date-outer div.boxim")
 
-    items match {
-      case Nil =>
-        ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
-        .withRequestTimeout(5000.millis)
-        .post(Json.obj("text" -> s"すみません！ キーワード「${keyword}」にマッチするイラストは見つかりませんでした..."))
-      case _ =>
-        val url = items.head >> attr("href")("a")
+      items match {
+        case Nil =>
+          ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
+            .withRequestTimeout(5000.millis)
+            .post(Json.obj("text" -> s"すみません！ キーワード「${keyword}」にマッチするイラストは見つかりませんでした..."))
+        case _ =>
+          val url = items.head >> attr("href")("a")
 
-        val doc2 = browser.get(url)
+          val doc2 = browser.get(url)
 
-        val elem = doc2 >> element("div.date-outer")
-        val imageTitleElem = elem >> element("div.title h2")
-        val images = elem >> elementList("div.entry a")
+          val elem = doc2 >> element("div.date-outer")
+          val imageTitleElem = elem >> element("div.title h2")
+          val images = elem >> elementList("div.entry a")
 
-        images match {
-          case Nil =>
-            ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
-              .withRequestTimeout(5000.millis)
-              .post(Json.obj("text" -> s"すみません！ キーワード「${keyword}」にマッチするイラストは見つかりませんでした..."))
-          case _ =>
-            // 複数ある場合はランダムに選択
-            val r = Random.nextInt(images.size)
-            var imageUrl: String = null
-            var index = 0
+          images match {
+            case Nil =>
+              ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
+                .withRequestTimeout(5000.millis)
+                .post(Json.obj("text" -> s"すみません！ キーワード「${keyword}」にマッチするイラストは見つかりませんでした..."))
+            case _ =>
+              // 複数ある場合はランダムに選択
+              val r = Random.nextInt(images.size)
+              var imageUrl: String = null
+              var index = 0
 
-            for (img <- images) {
-              if (index == r) imageUrl = img >> attr("src")("img")
-              index += 1
-            }
+              for (img <- images) {
+                if (index == r) imageUrl = img >> attr("src")("img")
+                index += 1
+              }
 
-            if (imageUrl.startsWith("//")) {
-              imageUrl = "https:" + imageUrl
-            }
+              if (imageUrl.startsWith("//")) {
+                imageUrl = "https:" + imageUrl
+              }
 
-            val payload = Json.obj(
-              "response_type" -> "in_channel",
-              "blocks" -> Json.arr(
-                Json.obj(
-                  "type" -> "image",
-                  "title" -> Json.obj(
-                    "type" -> "plain_text",
-                    "text" -> imageTitleElem.text,
-                    "emoji" -> true
-                  ),
-                  "image_url" -> imageUrl,
-                  "alt_text" -> imageTitleElem.text
+              val payload = Json.obj(
+                "response_type" -> "in_channel",
+                "blocks" -> Json.arr(
+                  Json.obj(
+                    "type" -> "image",
+                    "title" -> Json.obj(
+                      "type" -> "plain_text",
+                      "text" -> imageTitleElem.text,
+                      "emoji" -> true
+                    ),
+                    "image_url" -> imageUrl,
+                    "alt_text" -> imageTitleElem.text
+                  )
                 )
               )
-            )
 
-            ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
-              .withRequestTimeout(5000.millis)
-              .post(payload)
-        }
+              ws.url(responseUrl).addHttpHeaders("Content-Type" -> "application/json")
+                .withRequestTimeout(5000.millis)
+                .post(payload)
+          }
+      }
+    } catch {
+      case e:Exception => e.printStackTrace()
     }
   }
 }
